527a09e4bc1bcc0a8d8560a1cf14a3a87891db84
Eric Vallejos <131829048+PWS21Eric@users.noreply.github.com>
2023-11-04 00:08:15 +0000

    Update 2 files

---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style.css";
+
+export default class Boton extends React.component {
+    constructor (props) {
+        super(props);
+        this.state = {};       
+    }
+    render () {
+        return <button> </button>
+    }
+}
